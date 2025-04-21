d15f2892956104143474b9aca82feee9de80fdf7
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2025-04-21 23:40:36 +0700

    dix responsive[card]

--- asset/js/list-product.js
+++ asset/js/list-product.js
@@ -1,14 +1,19 @@
 
-fetch('./product.json') 
-    .then(res => res.json())
-    .then(data => {
-      const container = document.getElementById('sub-container');
+// fetch('./product.json')
+fetch('./product.json')  
+  .then(response => response.json())  
+  .then(data => {
+    const container = document.getElementById("sub-container"); // container card
+    const buttons = document.querySelectorAll("#content-sub a");
 
-      data.forEach(item => {
-        const cardHTML = `
-          <div class="col col-sm-1 col-md-4 col-lg-3" id="sub">
-            <div class="card" style="width: 18rem;">
-              <img src="${item.images[0].url}" class="card-img-top w-100" alt="sub${item.id}" style="height: 30vh;">
+    // render ข้อมูล
+    function renderCards(filteredData) {
+      container.innerHTML = ""; // ล้างก่อน
+      filteredData.forEach(item => {
+        container.innerHTML += `
+          <div class="col-sm-12 col-md-4 col-lg-3 mb-4">
+            <div class="card" style="width: 100%;">
+              <img src="${item.images[0].url}" class="card-img-top w-100" alt="${item.title}" style="height: 30vh;">
               <div class="card-body">
                 <h5 class="card-title">${item.title}</h5>
                 <p class="card-text">${item.text}</p>
@@ -20,7 +25,26 @@ fetch('./product.json')
             </div>
           </div>
         `;
-        container.insertAdjacentHTML('beforeend', cardHTML);
       });
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
+    }
+
+    buttons.forEach(btn => {
+      btn.addEventListener("click", e => {
+        e.preventDefault();
+        const value = btn.getAttribute("value");
+
+        
+        if (value === "all") {
+          renderCards(data); 
+        } else {
+          const filtered = data.filter(item => item.category.toLowerCase() === value);
+          renderCards(filtered); 
+        //   console.log(filtered)
+        }
+      });
+    });
+
+    // แสดงทั้งหมดเริ่มต้น
+    renderCards(data);
+  })
+  .catch(error => console.error('Error fetching data:', error));  
